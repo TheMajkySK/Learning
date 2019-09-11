060bc77d2323f3be01c0e7f82e1cb29ebebcdb4d
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-09-11 08:08:15 +0200

    training

---
+++ Python programs/Noob level/premenne.py
@@ -0,0 +1,13 @@
+a=4
+b=10
+c=23
+x=b-a
+y=a*b
+z=c+b
+p=x*y
+print(x)
+print(y)
+print(z)
+print(x-y)
+print(y+z)
+print(p)
---
+++ Python programs/Noob level/vypocty.py
@@ -0,0 +1,7 @@
+print(4+5)
+print(6-3)
+print(5/2)
+print(3*6)
+print(3%2)
+print(3//2)
+print(5**2)
