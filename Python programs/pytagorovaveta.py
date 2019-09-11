45d08a479ffe7f43b3e4e5062218a0155cdc80e3
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-09-11 08:43:21 +0200

    pytagorovavetaapretypovanie
    vytvoril som script na vypocitanie prepony c pomocou pytagorovej vety a naucil som sa pretypovať hodnoty

---
+++ Python programs/pytagorovaveta.py
@@ -0,0 +1,6 @@
+import math
+a=float(input("Zadaj stranu a: ")) #pretypovanie hodnoty a na float
+b=float(input("Zadaj stranu b: ")) #pretypovanie hodnoty b na float
+x=a**2+b**2
+c=math.sqrt(x)
+print("Prepona je ",c)
