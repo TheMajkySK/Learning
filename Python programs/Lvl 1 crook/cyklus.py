26c93d1fe229cd74560109ef9d195c2b68ff7a61
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-09-12 19:38:57 +0200

    asdasdsa

--- Python programs/Lvl 1 crook/cyklus.py
+++ Python programs/Lvl 1 crook/cyklus.py
@@ -2,3 +2,5 @@ for prem in range(10):
     print("python")
     print("KYS")
 print("======================")
+print()
+input("Press any ENTER to close program")
--- Python programs/Lvl 1 crook/vymenovanehodnoty.py
+++ Python programs/Lvl 1 crook/vymenovanehodnoty.py
@@ -2,3 +2,5 @@ sucin = 1
 for cislo in 1, 2, 3, 4, 5, 6:
     sucin = sucin * cislo
 print("6 faktorial =", sucin)
+print()
+input("Press any ENTER to close program")
