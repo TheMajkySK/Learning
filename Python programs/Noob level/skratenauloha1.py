ea1e5ee533e1132087b03fe1f833f57e15b581e4
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-09-12 12:57:22 +0200

    EURtoCZK

---
+++ Python programs/Noob level/skratenauloha1.py
@@ -0,0 +1,4 @@
+a = float(input("Zadaj eurá: "))
+print(a, "EUR je", a*float(25), "CZK")
+print()
+input("Press any ENTER to close program")
---
+++ Python programs/Noob level/uloha1.py
@@ -0,0 +1,5 @@
+a = float(input("Zadaj eurá: "))
+b = float(25) * a
+print("Suma", a, "EUR je", b, "CZK")
+print()
+input("Stlač ENTER pre zatvorenie programu")
