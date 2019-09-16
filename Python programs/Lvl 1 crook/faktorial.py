df6ef027d92a2bbf7186f92fbf8d38df7d508b42
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-09-16 10:29:52 +0200

    faktorial

---
+++ Python programs/Lvl 1 crook/faktorial.py
@@ -0,0 +1,7 @@
+n = int(input("Zadaj faktoriál: "))
+f = 1
+for i in range(1, n+1):
+    f = f * i
+print(f"{n}! = {f}")
+print()
+input("Press any ENTER to close program")
--- Python programs/Lvl 1 crook/sucet.py
+++ Python programs/Lvl 1 crook/sucet.py
@@ -3,3 +3,5 @@ sucet = 0
 for i in range(1, a + 1):
     sucet = sucet + i
 print("Súčet =", sucet)
+print()
+input("Press any ENTER to close program")
