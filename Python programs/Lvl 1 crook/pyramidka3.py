858d269b42769bcef5d29620bb5efd3286424a1b
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-09-23 11:07:47 +0200

    Create pyramidka3.py

---
+++ Python programs/Lvl 1 crook/pyramidka3.py
@@ -0,0 +1,7 @@
+n = int(input("Zadaj počet riadkov: "))
+for r in range(1, n + 1):
+    for m in range(1, n - r + 1):
+        print(" ", end = "")
+    for h in range (1, r + 1):
+        print("*", end="")
+    print()
