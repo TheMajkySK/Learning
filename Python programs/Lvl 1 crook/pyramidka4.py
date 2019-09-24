6c69e8a8f1a997ab7656fcfb1a20549d4b3c5e45
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-09-24 15:38:46 +0200

    Create pyramidka4.py

---
+++ Python programs/Lvl 1 crook/pyramidka4.py
@@ -0,0 +1,15 @@
+n = int(input("Zadaj počet riadkov: "))
+for r in reversed(range(1, n + 1)):
+    for m in range(0, n - r + 1):
+        print(" ", end=" " )
+    for h in range(1, r + 1):
+        print("* ", end="")
+    print()
+print
+print
+
+for r in reversed(range(0, n + 1)):
+    print("  " * (n-r) + "* " * r)
+
+print()
+input("Stlač ENTER pre ukončenie programu")
