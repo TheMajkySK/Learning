de4866f6228de21719d8f4e788ec65a8d32c75c0
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-09-25 09:11:27 +0200

    Update formatovanie.py

--- Python programs/Lvl 1 crook/formatovanie.py
+++ Python programs/Lvl 1 crook/formatovanie.py
@@ -1,4 +1,4 @@
 c = 15.3729
 print(f"Cislo {c:1.2f}")    #ak dam f pred uvodzovky tak možem pisať premenne do {}
                             #ak chcem formatovat premennu tak pišem c:1.2f čo znamena že sa
-                            # zapise ako cislo s 2 desatinnimy miestami
+                            # zapise ako cislo s 1 poziciou pred bodkou a s 2 desatinnimy miestami
