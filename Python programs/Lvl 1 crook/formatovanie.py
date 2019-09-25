835356c8d995d6a78e544374cf04d60c4d2d86d8
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-09-25 09:21:10 +0200

    Update formatovanie.py

--- Python programs/Lvl 1 crook/formatovanie.py
+++ Python programs/Lvl 1 crook/formatovanie.py
@@ -1,4 +1,7 @@
 c = 15.3729
-print(f"Cislo {c:1.2f}")    #ak dam f pred uvodzovky tak možem pisať premenne do {}
+print(f"Cislo {c:15.2f}")    #ak dam f pred uvodzovky tak možem pisať premenne do {}
                             #ak chcem formatovat premennu tak pišem c:1.2f čo znamena že sa
                             # zapise ako cislo s 1 poziciou pred bodkou a s 2 desatinnimy miestami
+print()
+x = "jano"
+print(f"Cislo {x:^20s}")    #^20s spravi to že odsadí text o 20 znakov dalej
