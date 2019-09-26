28990a84ad55c9e7b71c3859e268314255446969
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-09-26 13:52:14 +0200

    Create znamka.py

---
+++ Python programs/Lvl 1 crook/znamka.py
@@ -0,0 +1,9 @@
+#program ma načiat max počet bodov z testu potom získaný počet bodov
+#program vypočita percentualná testu a zistí či žiak dostal 5ku
+mx = int(input("Zadaj maximálny počet bodov: "))
+n = int(input("Zadaj počet získaných bodov: "))
+x = (n / mx) * 100
+if x >= 30:
+    print("Známka je lepšia ako 5")
+else:
+    print("Známka je 5")
