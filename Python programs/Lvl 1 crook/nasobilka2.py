821b86d9917bfbeb55ce02085c04fdd5e8259272
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-09-26 13:19:55 +0200

    Update nasobilka2.py

--- Python programs/Lvl 1 crook/nasobilka2.py
+++ Python programs/Lvl 1 crook/nasobilka2.py
@@ -1,7 +1,15 @@
 n = int(input("Zadaj číslo pre vytvorenie nasobilky: "))
+x = 0
+for i in range(1, n + 1):
+    x += 1
+    print(f"   {x:3}", end="")
+print()
+x = 0
 for r in range(1, n + 1):
+    x += 1
+    print(f"{x:3}", end="")
     for i in range(1, n + 1):
-        print(f"{r * i:2}", end=" ")
+        print(f"{r * i:3}", end=" ")
     print()
 print()
 input("Stlač ENTER pre ukončenie programu")
