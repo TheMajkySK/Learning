f95e2e298c9fddf4bbf85b036703455d331dfc53
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-10-02 09:21:59 +0200

    Create 31_mesiace.py

---
+++ Python programs/Lvl 1 crook/31_mesiace.py
@@ -0,0 +1,9 @@
+a = int(input("Zadaj číslo mesiaca: "))
+if a in {12,1,2}:
+    print("Zima")
+elif a in {3,4,5}:
+    print("Jar")
+elif a in {6,7,8}:
+    print("Leto")
+elif a in {9,10,11}:
+    print("Jeseň")
