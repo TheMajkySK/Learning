86171326d9da630d70464821c02058053656b9b6
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-10-07 11:10:31 +0200

    Update 36_if_opakovanie.py

--- Python programs/Lvl 1 crook/36_if_opakovanie.py
+++ Python programs/Lvl 1 crook/36_if_opakovanie.py
@@ -1,5 +1,8 @@
 b = int(input("Zadaj číslo: "))
+sucet = 0
 print("Delitele: ")
 for i in range(1, b + 1):
     if b % i == 0:
         print(f"{i}")
+        sucet += i
+print(f"Sučet delitelov je {sucet}")
