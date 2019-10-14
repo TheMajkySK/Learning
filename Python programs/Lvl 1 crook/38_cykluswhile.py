ef68b1bc1d26ffe5a9db8dcf403a7d97987fa2d6
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-10-14 10:32:39 +0200

    Update 38_cykluswhile.py

--- Python programs/Lvl 1 crook/38_cykluswhile.py
+++ Python programs/Lvl 1 crook/38_cykluswhile.py
@@ -8,28 +8,31 @@ nahodne_cislo = random.randint(1, 100)
 print("Budeš hádať číslo na ktoré myslím")
 print("Zadaj číslo v intervale <1, 100> a ja ti poviem či je väčšie alebo menšie než to na ktoré myslím: ", end="")
 cislo = 0
-pocet = 1
+pocet = 0
 while cislo <= nahodne_cislo or cislo >= nahodne_cislo:
     cislo = int(input())
     if cislo < nahodne_cislo:
         print("Zadaj väčšie číslo: ", end="")
+        pocet += 1
         continue
     elif cislo > nahodne_cislo:
         print("Zadaj menšie číslo: ", end="")
+        pocet += 1
         continue
     elif cislo == nahodne_cislo:
         print("Jes ma men uhadol si xD")
+        pocet += 1
         break
-    pocet += 1
-if pocet <= 20:
+print(f"Dal si to na {pocet} pokusov")
+if pocet == 1:
     print("Ta ty ši iny šef jako")
-elif pocet <= 15:
+elif pocet <= 5:
     print("Pjekneeeeeeee")
 elif pocet <= 10:
     print("Ta jako neši až taky plany")
-elif pocet <= 5:
+elif pocet <= 15:
     print("Zamysli sa nad svojim životom prosím")
-elif pocet == 1:
+elif pocet <= 20:
     print("Možeš rovno skočiť z okna")
 print()
 input("Stlač ENTER pre ukončenie programu")
