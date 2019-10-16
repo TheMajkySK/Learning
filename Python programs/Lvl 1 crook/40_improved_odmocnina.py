3331ca7f4e4d35f6bb6b4e60eb4605259b73b450
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-10-16 09:04:53 +0200

    Create 40_improved_odmocnina.py

---
+++ Python programs/Lvl 1 crook/40_improved_odmocnina.py
@@ -0,0 +1,14 @@
+cislo = float(input("Zadaj číslo pre výpočet odmocniny: "))
+od = 1
+do = cislo
+a = 0
+while abs(cislo-a**2) > 0.001:
+    if a**2 == cislo:
+        break
+    a = (od + do) / 2
+    if a**2 > cislo:
+        do = a
+    else:
+        od = a
+    a = (od + do) / 2
+print(f"Druhá odmocnina {cislo} = {a}")
