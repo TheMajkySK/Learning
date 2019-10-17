04eaea4075dbbfed62bd79532403e47bcc274723
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-10-17 13:15:48 +0200

    Update 40_improved_odmocnina.py

--- Python programs/Lvl 1 crook/40_improved_odmocnina.py
+++ Python programs/Lvl 1 crook/40_improved_odmocnina.py
@@ -2,7 +2,9 @@ cislo = float(input("Zadaj číslo pre výpočet odmocniny: "))
 od = 1
 do = cislo
 a = 0
+pocitadlo = 0
 while abs(cislo-a**2) > 0.001:
+    pocitadlo += 1
     if a**2 == cislo:
         break
     a = (od + do) / 2
@@ -12,3 +14,4 @@ while abs(cislo-a**2) > 0.001:
         od = a
     a = (od + do) / 2
 print(f"Druhá odmocnina {cislo} = {a}")
+print(f"Cyklus prebehol {pocitadlo}-krát")
