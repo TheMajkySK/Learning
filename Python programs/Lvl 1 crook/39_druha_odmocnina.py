4c4471b1dae17b1701e2d0e001e4aa8dabbe380e
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-10-17 13:15:50 +0200

    Update 39_druha_odmocnina.py

--- Python programs/Lvl 1 crook/39_druha_odmocnina.py
+++ Python programs/Lvl 1 crook/39_druha_odmocnina.py
@@ -4,6 +4,9 @@ volania funkcie math.sqrt(x)
 """
 cislo = int(input("Zadaj číslo na odmocnienie: "))
 a = 0
+pocitadlo = 0
 while a**2 < cislo:
+    pocitadlo += 1
     a += 0.01
 print(f"Druhá odmocnina čísla {cislo} = {a}")
+print(f"Cyklus prebehol {pocitadlo}-krát")
