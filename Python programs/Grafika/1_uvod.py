eb347897f26dc9fb6bc41175a4c1ddeb5ab417d6
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-10-21 11:12:19 +0200

    Update 1_uvod.py

--- Python programs/Grafika/1_uvod.py
+++ Python programs/Grafika/1_uvod.py
@@ -1,5 +1,5 @@
 import tkinter
-canvas = tkinter.Canvas()        
-canvas.pack()                       #toto musi mat kazdy program v ktorom pouzivame grafiku
-canvas.mainloop()
+canvas = tkinter.Canvas()  
+canvas.pack()                          #toto musi mat kazdy program v ktorom pouzivame grafiku
 canvas.create_line(10,10,50,70, fill="red")
+canvas.mainloop()
