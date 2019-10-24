a39bfb8e4e21507df572dc32dbc418d7480fd407
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-10-24 13:49:59 +0200

    Update 8_random_stvorec.py

--- Python programs/Grafika/8_random_stvorec.py
+++ Python programs/Grafika/8_random_stvorec.py
@@ -1,10 +1,12 @@
 import tkinter, random
-canvas = tkinter.Canvas(width = 1280, height = 720)
-for i in range(1,11):
-    x = random.randrange(200)
-    y = random.randrange(200)
-    velkost = random.randint(30, 100)
-    canvas.pack()
-    canvas.create_rectangle(x, y, x + velkost, y + velkost)
+canvas = tkinter.Canvas(width = 720, height = 720)
+canvas.pack()
+for i in range(1,1000):
+    farba = random.choice(("blue", "red", "magenta", "black", "white", "green"))
+    x = random.randrange(720)
+    y = random.randrange(720)
+    velkost = random.randint(30, 720)
+    canvas.create_rectangle(x, y, x + velkost, y + velkost, outline=farba, width=10)
     canvas.create_text(x + (velkost/2), y + (velkost/2), text=i, font="Arial 12 bold")
+    canvas.update()
 canvas.mainloop()
