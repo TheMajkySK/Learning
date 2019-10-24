d46f6d26d113a6a7592c7f9cc1a7be65073583c3
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-10-24 13:03:53 +0200

    Update 7_čiary.py

--- Python programs/Grafika/7_čiary.py
+++ Python programs/Grafika/7_čiary.py
@@ -2,7 +2,7 @@ import tkinter, random
 sucet = 0
 canvas = tkinter.Canvas(width = 1280, height = 720)
 canvas.pack()
-for i in range(10000):
+for i in range(100):
     canvas.create_line(0, 0, random.randrange(200), 100)
     sucet += 1
 canvas.mainloop()
