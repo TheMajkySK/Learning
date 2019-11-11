36bacf741f4d5a4a6d86e6e2d3c8b2376fe63f36
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-11-11 09:49:17 +0100

    Create 18_f_obsah_obdlznika.py

---
+++ Python programs/Grafika/18_f_obsah_obdlznika.py
@@ -0,0 +1,7 @@
+"""
+1. funkcia na vypocet obsahu obdlznika zo str. a,b
+2. vypocet obsahu plochy pod grafom nejakej funkcie na intervale <x1, x2>
+"""
+def Sobd(a,b):
+    return a*b
+print(Sobd(5,9))
