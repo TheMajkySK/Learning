1dafc014146d40aab1b1b0724dd384a7e090da41
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-11-27 08:28:46 +0100

    Create 6_chr.py

---
+++ Python programs/Gettin harder/6_chr.py
@@ -0,0 +1,10 @@
+for i in range(1,256):
+    print(chr(i), end=", ")   #chr() urobi z cisla znak
+"""
+ASCII je tabulka v ktorej je 128 cisel ku ktorym su priradene znaky @ - 64 ....... 1000000(v binarnom kode)
+                                                                    A - 65 ....... 1000001
+                                                                    a - 97
+WIN1250 - 256 znakov
+UTF-8
+UNICODE - 65536 znakov
+"""
