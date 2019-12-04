b8bfd39409e12e35324e75847534bd2193abd735
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-12-04 09:02:44 +0100

    asd

--- Python programs/Gettin harder/13_nove_funkcie.py
+++ Python programs/Gettin harder/13_nove_funkcie.py
@@ -2,8 +2,11 @@
 dalsie funkice:
 s.count(b) - pocet vyskytov podretazca b v retazci s
 s.find(b) - výstup bude index prveho vyskytu podrezaca b v retazci s
-strip(veta) - odstrani medzeri na začiatku a na konci retazca a tie aj ine oddelovace, napr. /n
+strip(veta) - odstrani medzery na začiatku a na konci retazca a tie aj ine oddelovace, napr. /n
 z = split(s) - rozdeli retazec na casti podla uvedeneho oddelovacieho znaku a vytvori zoznam stringov
 s.replace("a","***") - nahradi podretazec retazcom(pismeno a nahradi ***)
 formatovanie retazca 1.sposob : f"Dnes ma Jano {roky} rokov"
 """
+s = "Dnes je streda"
+print(s.count("s"))
+print(s.find("s"))
---
+++ Python programs/Gettin harder/14_zoznamy.py
@@ -0,0 +1,6 @@
+"""
+zoznamy(list)
+Zoznam je strukturovany datovy typ, ktory obsahuje postupnost dat rozneho typu.
+Je to menitelny typ, tzn. mozeme zmenit hodnotu konretneho prvku pomocou indexu.
+K jednotlivym polozkam zoznamu pristupujeme cez indexy v hranatych zatvorkach.
+"""
