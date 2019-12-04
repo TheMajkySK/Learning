4c045afbbc858f38a9e259d76ca716a59923b767
TheMajkySK <malcovsky.mikulas@gmail.com>
2019-12-04 09:07:44 +0100

    Update 13_nove_funkcie.py

--- Python programs/Gettin harder/13_nove_funkcie.py
+++ Python programs/Gettin harder/13_nove_funkcie.py
@@ -2,11 +2,13 @@
 dalsie funkice:
 s.count(b) - pocet vyskytov podretazca b v retazci s
 s.find(b) - výstup bude index prveho vyskytu podrezaca b v retazci s
-strip(veta) - odstrani medzery na začiatku a na konci retazca a tie aj ine oddelovace, napr. /n
-z = split(s) - rozdeli retazec na casti podla uvedeneho oddelovacieho znaku a vytvori zoznam stringov
+s.strip() - odstrani medzery na začiatku a na konci retazca a tie aj ine oddelovace, napr. /n
+s.split() - rozdeli retazec na casti podla uvedeneho oddelovacieho znaku a vytvori zoznam stringov
 s.replace("a","***") - nahradi podretazec retazcom(pismeno a nahradi ***)
 formatovanie retazca 1.sposob : f"Dnes ma Jano {roky} rokov"
 """
 s = "Dnes je streda"
 print(s.count("s"))
 print(s.find("s"))
+print(s.split(" "))
+print(s.replace("e", "***"))
