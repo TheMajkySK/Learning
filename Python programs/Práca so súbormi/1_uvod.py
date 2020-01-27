8b8f375df077c45830b484c4dd7e7404d0cec342
TheMajkySK <malcovsky.mikulas@gmail.com>
2020-01-27 09:48:55 +0100

    Create 1_uvod.py

---
+++ Python programs/Práca so súbormi/1_uvod.py
@@ -0,0 +1,5 @@
+"""
+Textovy subor - Je postupnost znakov, ktora moze obsahovat aj znaky konca raidka. Väčšinou je tato postupnost ulozena na disku v subore.
+Na textovy subor sa možeme pozerat ako na postupnost riadkov(može to byt aj prazdna postupnost), pricom kazdy riadok je postupnostou znakov (znakovy retazec), ktora je ukoncena
+specialnym znakom "\n"
+"""
