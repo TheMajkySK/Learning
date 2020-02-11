c92a96be16e1a01a1bc5bed5f86b6af622f4c4e1
TheMajkySK <malcovsky.mikulas@gmail.com>
2020-02-11 16:44:37 +0100

    Update TBCalcl.py

--- Python programs/TBCalc/TBCalcl.py
+++ Python programs/TBCalc/TBCalcl.py
@@ -1,15 +1,27 @@
 import tkinter
 
-def kil(n,j,d):
-    v = (j-n) // d
+def kil():
+    n = p_najaz.get()
+    ch = p_chc.get()
+    d = p_dni.get()
+    nch = int(ch)-int(n)
 
-    label1 = tkinter.Label()
+    label1 = tkinter.Label(text= int(nch)/int(d),font= "Arial 25 bold")
+    canvas.create_window(300,160,window= label1)
 
-canvas = tkinter.Canvas(width=1000, height=500)
+canvas = tkinter.Canvas(width=700, height=300)
 canvas.pack()
 
-canvas.create_text(188, 20, text = "Počet najazdených KM:", font = "Arial 25 bold")
-canvas.create_text(175, 55, text = "Koľko chceš najazdiť:", font = "Arial 25 bold")
-entry1 = tkinter.Entry()
-canvas.create_window(450, 20, window = entry1)
+canvas.create_text(188, 20, text= "Počet najazdených KM:", font= "Arial 25 bold")
+canvas.create_text(175, 55, text= "Koľko chceš najazdiť:", font= "Arial 25 bold")
+canvas.create_text(168, 90, text= "Za počet daných dní:", font= "Arial 25 bold")
+p_najaz = tkinter.Entry()
+p_chc = tkinter.Entry()
+p_dni = tkinter.Entry()
+canvas.create_window(450, 20, window= p_najaz)
+canvas.create_window(450,55,window= p_chc)
+canvas.create_window(450,90,window= p_dni)
+
+button = tkinter.Button(text=f"Výpočet KM na daný počet dní", command=kil)
+canvas.create_window(600,55,window=button)
 canvas.mainloop()
