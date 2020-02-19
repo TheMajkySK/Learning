f83da69357650c454e1d48e0b90fa91f29aa1ded
TheMajkySK <malcovsky.mikulas@gmail.com>
2020-02-19 08:15:31 +0100

    hjk

--- Python programs/Maturitne zadania/u02.py
+++ Python programs/Maturitne zadania/u02.py
@@ -1,3 +1,21 @@
 # === Úloha 2===
 # Napíšte program, ktorý  nájde v súbore vzor.txt najdlhšie slovo, vypíše ho a vypíše aj číslo riadka, na ktorom sa nachádza.
+from codecs import open
 
+subor = open("vzor.txt", "r", "utf-8")
+riadky = subor.readlines()
+
+najdlhsie_slovo = ""
+riadok_najdlhsieho = 0
+
+for i in range(len(riadky)):
+    s_riadky = riadky[i].strip()
+    slova = s_riadky.split(" ")
+
+    for slovo in slova:
+        if len(slovo) >= len(najdlhsie_slovo):
+            najdlhsie_slovo = slovo
+            riadok_najdlhsieho = i+1
+
+print(f"Najdlhšie slovo je {najdlhsie_slovo}")
+print(f"Je na riadku {riadok_najdlhsieho}")
--- Python programs/Teoria/poznamky.md
+++ Python programs/Teoria/poznamky.md
@@ -1,5 +1,7 @@
 # Siete
 - IP adresa - 4 miestne číslo, identifikator pocitaca v sieti, od 0-255, napr. 192.164.255.0
+- maska rozdeluje IP adresu tak, že prvá čast je cislo siete a druha cast cislo pocitaca
+
 - MAC adresa - hardverova adresa sietovej karty v zariadeni (napr. mobil, PC, router,...)
     - napr. H3:56:E5:56:AS:5P
     - prve 3 cisla - vyrobca, dalsie 3 cisla - cislo karty u vyrobcu
